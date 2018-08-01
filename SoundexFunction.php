103993b43cb88d1267b45268f8a928fa0ef93411
SLITI Brahim <brahim.sliti@supinfo.com>
2018-08-01 16:23:51 +0200

    Add files via upload

---
+++ SoundexFunction.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AppBundle\DQL;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\Parser;
+use Doctrine\ORM\Query\SqlWalker;
+
+/**
+ * SoundexFunction ::= "SOUNDEX" "(" StringPrimary ")".
+ */
+class SoundexFunction extends FunctionNode
+{
+    public $stringExpression = null;
+
+    public function parse(Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $this->stringExpression = $parser->StringPrimary();
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+
+    public function getSql(SqlWalker $sqlWalker)
+    {
+        return sprintf(
+            'soundex(%s)',
+            $this->stringExpression->dispatch($sqlWalker)
+        );
+    }
+}
